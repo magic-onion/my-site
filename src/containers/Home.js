e3c6be8bf495d33dfe3ecaca8babb196e864a961
magic-onion <alexander.m.truong@gmail.com>
2019-04-06 15:45:05 -0400

    adding todos

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -15,4 +15,7 @@ class Home extends React.Component {
 export default Home
 
 //elevator pitch and what i am tryna to do
-//
+// add Iframes for Projects
+// choose which projects you want up!
+//Write out a long about me
+//post resume as PDF  (version without phone)
